e66e3d3c399d76e7077f20f3af85583d45a802b8
John Page <jdehpage@gmail.com>
2019-01-09 10:38:07 +0000

    3nd Homework Update

--- ruby_functions_practice.rb
+++ ruby_functions_practice.rb
@@ -1,79 +1,81 @@
-# def return_10()
-#   result = 10
+require "date"
+
+def return_10()
+  result = 10
+end
+
+def add(no_one, no_two)
+  result = no_one + no_two
+  return result #Remember to explicitly return
+end
+
+def subtract(no_one, no_two)
+  return no_one - no_two
+end
+
+def multiply(no_one, no_two)
+  return no_one * no_two
+end
+
+def divide(no_one, no_two)
+  return no_one / no_two
+end
+
+
+def length_of_string(string_to_measure)
+  return string_to_measure.length
+end
+
+def join_string(string_1, string_2)
+  return string_1 + string_2
+end
+
+def add_string_as_number(string_1, string_2)
+  return string_1.to_i + string_2.to_i
+end
+
+
+
+def number_to_full_month_name(month_number)
+  return Date::MONTHNAMES[month_number]
+end
+
+
+ # return "January" if month_number == 1
+ # return "March" if month_number == 3
+ # return "September" if month_number == 9
 # end
-#
-# def add(no_one, no_two)
-#   result = no_one + no_two
-#   return result #Remember to explicitly return
+
+  # if month_number == 1
+  #   return "January"
+  # elsif
+  #   month_number == 3
+  #    return "March"
+  # elsif
+  #   month_number == 9
+  #     return "September"
+  # end
 # end
-#
-# def subtract(no_one, no_two)
-#   return no_one - no_two
+
+
+
+def number_to_short_month_name(month_number)
+  return Date::ABBR_MONTHNAMES[month_number]
+  # return number_to_short_month_name(month_number)[0..2]
+end
+
+
+#   if month_number == 1
+#     result = "Jan"
+#   elsif
+#     month_number == 4
+#      result = "Apr"
+#   elsif
+#     month_number == 10
+#       result = "Oct"
+#   end
 # end
-#
-# def multiply(no_one, no_two)
-#   return no_one * no_two
-# end
-#
-# def divide(no_one, no_two)
-#   return no_one / no_two
-# end
-#
-#
-# def length_of_string(string_to_measure)
-#   return string_to_measure.length
-# end
-#
-# def join_string(string_1, string_2)
-#   return string_1 + string_2
-# end
-#
-# def add_string_as_number(string_1, string_2)
-#   return string_1.to_i + string_2.to_i
-# end
-#
-#
-#
-# def number_to_full_month_name(month_number)
-#   return Date::MONTHNAMES[month_number]
-# end
-#
-#
-#  # return "January" if month_number == 1
-#  # return "March" if month_number == 3
-#  # return "September" if month_number == 9
-# # end
-#
-#   # if month_number == 1
-#   #   return "January"
-#   # elsif
-#   #   month_number == 3
-#   #    return "March"
-#   # elsif
-#   #   month_number == 9
-#   #     return "September"
-#   # end
-# # end
-#
-#
-#
-# def number_to_short_month_name(month_number)
-#   return Date::ABBR_MONTHNAMES[month_number]
-#   # return number_to_short_month_name(month_number)[0..2]
-# end
-#
-#
-# #   if month_number == 1
-# #     result = "Jan"
-# #   elsif
-# #     month_number == 4
-# #      result = "Apr"
-# #   elsif
-# #     month_number == 10
-# #       result = "Oct"
-# #   end
-# # end
-#
+
 
 
 
@@ -83,12 +85,13 @@ end
 
 
 def volume_of_sphere(radius)
-  result = (1.33)* (3.14)* (radius**3)
-  return result.round(5)
+  # result = (1.33)* (3.14)* (radius**3)
+  # return result.round(5)
+  return ((4.0 / 3) * Math::PI * (radius ** 3)).to_i
 end
 
 
-def fahrenheit_to_celsius(f)
-  result = ((f-32)*5.0)/9
+def fahrenheit_to_celsius(farenheit)
+  result = ((farenheit-32)*5.0)/9.0
   return result.round(5)
 end
